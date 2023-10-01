7f9b1e70c0823f9e30f95b8a8201a5eab53df965
AndresEscobar09 <wilmera.escobarc@ecci.edu.co>
2023-10-01 15:45:33 -0500

    se agrega codigp àra verificar si la palabra ingresada es un palindromo

---
+++ palindromo.py
@@ -0,0 +1,18 @@
+
+x = 1
+while (x>0):
+
+    palabra = input('ingrese una palabra para verificar si es un palindromo\n')
+    palabra1 = ''
+    x = len(palabra)
+
+
+    for i in range(x-1,-1,-1):
+        palabra1 = palabra1 + palabra[i]
+
+    if palabra == palabra1 and len(palabra) > 1:
+        print('la palabra "',palabra,'" es un palindromo\n')
+    else:
+        print('la palabra "',palabra,'" no es un palindromo\n')
+
+quit()
